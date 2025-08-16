19fbb2d9a5e808c2a2a8351ef89c391fe85c921c
NjilaBoris <njilaboris2@gmail.com>
2025-08-17 00:55:16 +0100

    Implement Hero Section

--- src/sections/ComingSoon.jsx
+++ src/sections/ComingSoon.jsx
@@ -12,9 +12,9 @@ const ComingSoon = () => {
           </h3>
         </div>
 
-        <div className="flex-center gap-10">
-          <img src="/images/ps-logo.svg" className="md:w-32 w-20" />
-          <img src="/images/x-logo.svg" className="md:w-52 w-40" />
+        <div className="flex-center gap-10 ">
+          <img src="/images/ps-logo.svg" className="md:w-30 w-20" />
+          <img src="/images/x-logo.svg" className="md:w-50 w-40" />
         </div>
       </div>
     </section>
